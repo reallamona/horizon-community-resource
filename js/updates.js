07f3b74feb6d11f886e1ea6077d9193abab34231
Lamona <lamona.content@gmail.com>
2026-07-27 01:46:09 -0500

    Update updates.js

--- js/updates.js
+++ js/updates.js
@@ -1,6 +1,15 @@
 const pageUpdates = {
-    "home.html": "July 26, 2026",
-    "community.html": "July 26, 2026",
-    "research.html": "July 20, 2026",
-    "speedrunning.html": "July 25, 2026"
+    "home.html": "July 27, 2026",
+    "community.html": "July 27, 2026",
+    "research.html": "July 27, 2026",
+    "speedrunning.html": "July 27, 2026",
+
+    "horizon-zero-dawn.html": "July 27, 2026",
+    "frozen-wilds.html": "July 27, 2026",
+    "zero-dawn-remastered.html": "July 27, 2026",
+    "horizon-forbidden-west.html": "July 27, 2026",
+    "burning-shores.html": "July 27, 2026",
+    "call-of-the-mountain.html": "July 27, 2026",
+    "lego-horizon-adventures.html": "July 27, 2026"
+    "hunters-gathering.html": "July 27, 2026"
 };
