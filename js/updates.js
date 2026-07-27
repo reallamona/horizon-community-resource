43561a6c61bcfd6c93fc21032d7ea3100964a516
Lamona <lamona.content@gmail.com>
2026-07-27 01:25:17 -0500

    Update updates.js

--- js/updates.js
+++ js/updates.js
@@ -1,1 +1,6 @@
-
+const pageUpdates = {
+    "home.html": "July 26, 2026",
+    "community.html": "July 26, 2026",
+    "research.html": "July 20, 2026",
+    "speedrunning.html": "July 25, 2026"
+};
