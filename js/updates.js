e18be3827a835b622f620308451d7597655de9b4
Lamona <lamona.content@gmail.com>
2026-07-31 15:11:24 -0500

    Update updates.js

--- js/updates.js
+++ js/updates.js
@@ -1,20 +1,16 @@
 const pageUpdates = {
 
-     // Main
-    
     "home.html": "July 31, 2026",
     "speedrunning.html": "July 28, 2026",
-     "community.html": "July 28, 2026",
+    "community.html": "July 28, 2026",
 
-     // Games
-    
-    "horizon-zero-dawn.html": "July 27, 2026",
-    "frozen-wilds.html": "July 27, 2026",
-    "zero-dawn-remastered.html": "July 27, 2026",
-    "horizon-forbidden-west.html": "July 27, 2026",
-    "burning-shores.html": "July 27, 2026",
-    "call-of-the-mountain.html": "July 27, 2026",
-    "lego-horizon-adventures.html": "July 27, 2026",
-    "hunters-gathering.html": "July 27, 2026",
-    "steel-frontiers.html": "July 27, 2026"
+    "games/horizon-zero-dawn.html": "July 27, 2026",
+    "games/frozen-wilds.html": "July 27, 2026",
+    "games/zero-dawn-remastered.html": "July 27, 2026",
+    "games/horizon-forbidden-west.html": "July 27, 2026",
+    "games/burning-shores.html": "July 27, 2026",
+    "games/call-of-the-mountain.html": "July 27, 2026",
+    "games/lego-horizon-adventures.html": "July 27, 2026",
+    "games/hunters-gathering.html": "July 27, 2026",
+    "games/steel-frontiers.html": "July 27, 2026"
 };
