31360324c28a989562d73061613ea05e3758e601
Lamona <lamona.content@gmail.com>
2026-08-02 22:36:58 -0500

    Update updates.js

--- js/updates.js
+++ js/updates.js
@@ -1,18 +1,18 @@
 const pageUpdates = {
 
     // Main Pages
-    "home.html": "July 31, 2026",
-    "speedrunning.html": "July 28, 2026",
-    "community.html": "July 28, 2026",
+    "home.html": "August 2, 2026",
+    "speedrunning.html": "August 2, 2026",
+    "community.html": "August 2, 2026",
 
     // Games
-    "games/horizon-zero-dawn.html": "July 27, 2026",
-    "games/frozen-wilds.html": "July 27, 2026",
-    "games/zero-dawn-remastered.html": "July 27, 2026",
-    "games/horizon-forbidden-west.html": "July 27, 2026",
-    "games/burning-shores.html": "July 27, 2026",
-    "games/call-of-the-mountain.html": "July 27, 2026",
-    "games/lego-horizon-adventures.html": "July 27, 2026",
-    "games/hunters-gathering.html": "July 27, 2026",
-    "games/steel-frontiers.html": "July 27, 2026"
+    "games/horizon-zero-dawn.html": "August 2, 2026",
+    "games/frozen-wilds.html": "August 2, 2026",
+    "games/zero-dawn-remastered.html": "August 2, 2026",
+    "games/horizon-forbidden-west.html": "August 2, 2026",
+    "games/burning-shores.html": "August 2, 2026",
+    "games/call-of-the-mountain.html": "August 2, 2026",
+    "games/lego-horizon-adventures.html": "August 2, 2026",
+    "games/hunters-gathering.html": "August 2, 2026",
+    "games/steel-frontiers.html": "August 2, 2026"
 };
