b4104fea25bc54ca2712aa79bda61e0595a2f88f
Lamona <lamona.content@gmail.com>
2026-08-02 22:28:44 -0500

    Update games.js

--- js/games.js
+++ js/games.js
@@ -1,1 +1,35 @@
+function setupGamesDropdown() {
 
+    const button =
+        document.querySelector(".nav-category > a");
+
+    const menu =
+        document.querySelector(".submenu");
+
+
+    if (!button || !menu) return;
+
+
+    button.addEventListener("click", e => {
+
+        e.preventDefault();
+
+        menu.style.display =
+            menu.style.display === "flex"
+            ? "none"
+            : "flex";
+
+    });
+
+
+    document.addEventListener("click", e => {
+
+        if (!e.target.closest(".nav-category")) {
+
+            menu.style.display = "none";
+
+        }
+
+    });
+
+}
