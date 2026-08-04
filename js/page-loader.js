ef9717d0c36bae2f30d265590840d8ad746221c0
Lamona <lamona.content@gmail.com>
2026-08-04 03:22:41 -0500

    Update page-loader.js

--- js/page-loader.js
+++ js/page-loader.js
@@ -1,1 +1,45 @@
+/* =========================
+   Page Loader
+========================= */
 
+async function loadPage(page) {
+
+    const content = document.getElementById("content");
+
+    content.innerHTML = `
+        <p>Loading...</p>
+    `;
+
+    try {
+
+        const response = await fetch(page);
+
+        if (!response.ok) {
+            throw new Error(`${page} failed to load`);
+        }
+
+        content.innerHTML = await response.text();
+       
+       initializePage(page);
+
+    } catch (error) {
+
+
+        console.error(error);
+
+    }
+
+}
+
+
+/* =========================
+   Page Initialization
+========================= */
+
+function initializePage(page) {
+
+    addCopyButtons();
+    addLastUpdated(page);
+    setupGamesDropdown();
+
+}
