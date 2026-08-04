bc8852c558b3a9859300b5a112cf67ecf479bf68
Lamona <lamona.content@gmail.com>
2026-08-04 03:05:03 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,6 +4,15 @@
 
 const CONFIG = {
     homePage: "pages/home.html",
+
+    pages: {
+        home: "pages/home.html",
+        games: "pages/games.html",
+        research: "pages/research.html",
+        speedrunning: "pages/speedrunning.html",
+        community: "pages/community.html"
+    },
+
     copyTimeout: 1000
 };
 
