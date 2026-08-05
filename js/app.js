dc5539e933539d859d91bcf1ccef2dbfcad22b36
Lamona <lamona.content@gmail.com>
2026-08-04 22:59:38 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -26,42 +26,6 @@ const STATE = {
 };
 
 
-/* =========================
-   Navigation
-========================= */
-
-function setActiveLink(link) {
-
-    document.querySelectorAll("[data-page]")
-        .forEach(item => {
-            item.classList.remove("active");
-        });
-
-    if (link) {
-        link.classList.add("active");
-    }
-
-}
-
-function setupNavigation() {
-
-    document.querySelectorAll("[data-page]").forEach(link => {
-
-        link.addEventListener("click", function (e) {
-
-            e.preventDefault();
-
-            const page = this.dataset.page;
-
-            loadPage(page);
-            setActiveLink(this);
-
-        });
-
-    });
-
-}
-
 
 /* =========================
    Initialize
