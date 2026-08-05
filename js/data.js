4260d77f7491fe60eb2151e342590c23f7d40f16
Lamona <lamona.content@gmail.com>
2026-08-04 23:07:26 -0500

    Delete js/data.js

--- js/data.js
+++
@@ -1,9 +0,0 @@
-export async function loadData(file) {
-    const response = await fetch(file);
-
-    if (!response.ok) {
-        throw new Error(`Couldn't load ${file}`);
-    }
-
-    return response.json();
-}
