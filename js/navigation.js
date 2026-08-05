c3c0a68941af0dd477c55399a385c6be1b57b8d5
Lamona <lamona.content@gmail.com>
2026-08-04 22:59:51 -0500

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,1 +1,35 @@
+/* =========================
+   Navigation
+========================= */
 
+function setActiveLink(link) {
+
+    document.querySelectorAll("[data-page]")
+        .forEach(item => {
+            item.classList.remove("active");
+        });
+
+    if (link) {
+        link.classList.add("active");
+    }
+
+}
+
+function setupNavigation() {
+
+    document.querySelectorAll("[data-page]").forEach(link => {
+
+        link.addEventListener("click", function (e) {
+
+            e.preventDefault();
+
+            const page = this.dataset.page;
+
+            loadPage(page);
+            setActiveLink(this);
+
+        });
+
+    });
+
+}
