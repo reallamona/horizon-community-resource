7d8e3f8791778fe2350b3524a17197a2c53b1768
Lamona <lamona.content@gmail.com>
2026-08-06 19:20:55 -0500

    Update updates.js

--- js/updates.js
+++ js/updates.js
@@ -5,22 +5,22 @@
 const pageUpdates = {
 
     // Main Pages
-    "pages/home.json": "August 4, 2026",
-    "pages/speedrunning.json": "August 2, 2026",
-    "pages/community.json": "August 2, 2026",
-    "pages/games.json": "August 2, 2026",
+    "data/pages/home.json": "August 4, 2026",
+    "data/pages/speedrunning.json": "August 2, 2026",
+    "data/pages/community.json": "August 2, 2026",
+    "data/pages/games.json": "August 2, 2026",
 
 
     // Games
-    "games/horizon-zero-dawn.json": "August 2, 2026",
-    "games/frozen-wilds.json": "August 2, 2026",
-    "games/zero-dawn-remastered.json": "August 2, 2026",
-    "games/horizon-forbidden-west.json": "August 2, 2026",
-    "games/burning-shores.json": "August 2, 2026",
-    "games/call-of-the-mountain.json": "August 2, 2026",
-    "games/lego-horizon-adventures.json": "August 2, 2026",
-    "games/hunters-gathering.json": "August 2, 2026",
-    "games/steel-frontiers.json": "August 2, 2026"
+    "data/games/horizon-zero-dawn.json": "August 2, 2026",
+    "data/games/frozen-wilds.json": "August 2, 2026",
+    "data/games/zero-dawn-remastered.json": "August 2, 2026",
+    "data/games/horizon-forbidden-west.json": "August 2, 2026",
+    "data/games/burning-shores.json": "August 2, 2026",
+    "data/games/call-of-the-mountain.json": "August 2, 2026",
+    "data/games/lego-horizon-adventures.json": "August 2, 2026",
+    "data/games/hunters-gathering.json": "August 2, 2026",
+    "data/games/steel-frontiers.json": "August 2, 2026"
 
 };
 
@@ -35,6 +35,17 @@ function addLastUpdated(page) {
 
     if (!updated) return;
 
+
+    const update = pageUpdates[page];
+
+
+    if (update) {
+
+        updated.textContent = `Last Updated: ${update}`;
+
+    }
+
+}
 
     const update = pageUpdates[page];
 
